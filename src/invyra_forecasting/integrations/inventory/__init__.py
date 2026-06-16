81e02a06536d300e4091bc1331b6de927727400b
chrykoolaid <atillacokyavuz1992@gmail.com>
2026-06-16 23:44:23 +0800

    Expose Phase 2A inventory adapter mapping API

---
+++ src/invyra_forecasting/integrations/inventory/__init__.py
@@ -0,0 +1,23 @@
+from invyra_forecasting.integrations.inventory.adapter import (
+    InventoryAdapterMappingError,
+    InventoryForecastMapper,
+    InventoryForecastMappingInput,
+    InventoryItemRecord,
+    InventoryLocationRecord,
+    InventoryMovementLedgerRecord,
+    InventoryStockPositionRecord,
+    InventorySupplierProfileRecord,
+    map_inventory_to_forecast_request,
+)
+
+__all__ = [
+    "InventoryAdapterMappingError",
+    "InventoryForecastMapper",
+    "InventoryForecastMappingInput",
+    "InventoryItemRecord",
+    "InventoryLocationRecord",
+    "InventoryMovementLedgerRecord",
+    "InventoryStockPositionRecord",
+    "InventorySupplierProfileRecord",
+    "map_inventory_to_forecast_request",
+]
