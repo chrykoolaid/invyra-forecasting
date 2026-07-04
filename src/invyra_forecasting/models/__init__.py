a5bb7583c4907495454367210a21e065cbc3e4b7
chrykoolaid <atillacokyavuz1992@gmail.com>
2026-07-04 23:31:34 +0800

    Phase 5D export model orchestration foundation

--- src/invyra_forecasting/models/__init__.py
+++ src/invyra_forecasting/models/__init__.py
@@ -1,6 +1,15 @@
 from invyra_forecasting.models.baseline import BaselineExplainableDemandModel
 from invyra_forecasting.models.contracts import ForecastModelInput, ForecastModelOutput
 from invyra_forecasting.models.handoff import ForecastModelHandoffAdapter
+from invyra_forecasting.models.orchestration import (
+    ForecastModelOrchestrator,
+    ForecastModelRegistry,
+    ModelLifecycleStatus,
+    ModelSelectionResult,
+    OrchestratedForecastResult,
+    RegisteredForecastModel,
+    build_default_model_registry,
+)
 from invyra_forecasting.models.service import ForecastModelService
 from invyra_forecasting.models.simple import SimpleDemandForecaster
 
@@ -8,7 +17,14 @@ __all__ = [
     "BaselineExplainableDemandModel",
     "ForecastModelHandoffAdapter",
     "ForecastModelInput",
+    "ForecastModelOrchestrator",
     "ForecastModelOutput",
+    "ForecastModelRegistry",
     "ForecastModelService",
+    "ModelLifecycleStatus",
+    "ModelSelectionResult",
+    "OrchestratedForecastResult",
+    "RegisteredForecastModel",
     "SimpleDemandForecaster",
+    "build_default_model_registry",
 ]
